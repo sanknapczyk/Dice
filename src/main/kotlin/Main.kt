a3ca99cd2d39d9fa31b9defffe9f5daa4e90c404
sanknapczyk <106260690+sanknapczyk@users.noreply.github.com>
2023-08-16 21:51:50 +0100

    Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,50 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val users = mapOf("Sandra1" to "qwerty1", "Sandra2" to "qwerty2")
+    var maxAttempts = 3
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("Welcome to DICE GAME!")
+    while (true) {
+        println("Enter your login: ")
+        val username = readlnOrNull()
+        println("Enter your password: ")
+        val password = readlnOrNull()
+
+        if (username != null && password != null) {
+            val storedPassword = users[username]
+            if(password==storedPassword){
+                println("Login successful.\nHow many time does it take to roll number 6? Let's find out!")
+                diceGame()
+                break
+            }else{
+                maxAttempts--
+                if(maxAttempts>0){
+                    println("Invalid password or username. Please try again\nAttempts to try left: $maxAttempts")
+                }else{
+                    println("Max amount of attempts to login exceeded.")
+                    break
+                }
+            }
+        }
+    }
 }
+
+fun diceGame(){
+    var diceFace: Int
+    var rollCount=1
+    diceFace  = (1..6).random()
+
+    if (diceFace!=6) {
+        println("Dice number is: $diceFace. To roll again press enter. ")
+        readlnOrNull()
+        while (diceFace!=6){
+            println("Dice number is: $diceFace")
+            diceFace  = (1..6).random()
+            rollCount++
+        }
+        println("Dice number is: $diceFace. Congrats you rolled 6!\nGame over")
+        println("It took $rollCount times to roll 6.")
+    } else{
+        println("Dice number is: $diceFace. You rolled 6!")
+    }
+}
+
